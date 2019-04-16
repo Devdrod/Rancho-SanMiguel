10a351a0c4602439820e71bbb998af234b3d9fa3
David Rodriguez <devdrod@gmail.com>
2019-04-16 14:58:57 -0600

    new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Create-react-app + Bootstrap + SASS ENVIROMENT</h1>
+    );
+  }
+}
+
+export default App;
